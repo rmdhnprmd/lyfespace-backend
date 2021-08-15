05d547598ed175bd78885738d36d606f61ff2dc8
rmdhnprmd <ramadhan.permadi36@gmail.com>
2021-08-15 16:57:06 +0700

    create blueprint products database

--- database/migrations/2021_08_15_095059_create_products_table.php
+++ database/migrations/2021_08_15_095059_create_products_table.php
@@ -15,6 +15,13 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('users_id');
+            $table->integer('categories_id');
+            $table->integer('price');
+            $table->longText('description');
+            
+            $table->softDeletes();
             $table->timestamps();
         });
     }
