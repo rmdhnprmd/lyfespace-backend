0447f93266ad0eb3bf1d505031e149f60eb30b7a
rmdhnprmd <ramadhan.permadi36@gmail.com>
2021-09-29 13:44:12 +0700

    Integrasi Index Page Dashboard My Product

--- app/Http/Controllers/DashboardProductController.php
+++ app/Http/Controllers/DashboardProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardProductController extends Controller
 {
@@ -13,7 +15,13 @@ class DashboardProductController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard-products');
+        $products = Product::with(['galleries', 'category'])
+                    ->where('users_id', Auth::user()->id)
+                    ->get();
+
+        return view('pages.dashboard-products', [
+            'products' => $products
+        ]);
     }
 
     public function details()
